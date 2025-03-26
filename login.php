7767cfabe50c28c3a3ab096479cc459b8b50b905
yaor3n <lokeyaoren123@gmail.com>
2025-03-26 16:33:24 +0800

    update

--- login.php
+++ login.php
@@ -80,18 +80,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </section>
     </div>
 
-    <script src="stuff/login.js">
-            // Check if PHP session has username
-    <?php if (!empty($_SESSION["username"])): ?>
-        localStorage.setItem("loggedInUser", "<?php echo htmlspecialchars($_SESSION['username']); ?>");
-    <?php endif; ?>
-
-    // Show password toggle
-    document.getElementById("show-password").addEventListener("change", function () {
-        var passwordField = document.getElementById("password");
-        passwordField.type = this.checked ? "text" : "password";
-    });
-        
-    </script>
+    <script src="stuff/login.js"></script>
 </body>
 </html>
